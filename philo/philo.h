8b2b3f0a73d86a23fcf57b96d9272edb98c473b2
fatheddine-bicane <fatheddine04@gmail.com>
2025-03-07 00:08:31 +0000

    testing commits

---
+++ philo/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 17:45:08 by fbicane           #+#    #+#             */
+/*   Updated: 2025/03/06 21:35:16 by fbicane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h> //usleep
+# include <stdio.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <pthread.h> //mutex, thread
+# include <sys/time.h> //gettimeofday
+
+
+
+#endif
