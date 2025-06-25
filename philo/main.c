33b728236c5f5e1e104e23354891e2bde4f289f5
fatheddine-bicane <fatheddine04@gmail.com>
2025-06-25 22:14:22 +0100

    fix: add dinner_simulation to main

--- philo/main.c
+++ philo/main.c
@@ -28,6 +28,8 @@ int	main(int argc, char **argv)
 			return (EXIT_FAILURE);
 		if (false == data_init(&table))
 			return (EXIT_FAILURE);
+		if (false == dinner_simulation(&table))
+			return (EXIT_FAILURE);
 		/*printf("%ld\n", table.philo_nbr);*/
 		/*printf("%ld\n", table.time_to_die);*/
 		/*printf("%ld\n", table.time_to_eat);*/
