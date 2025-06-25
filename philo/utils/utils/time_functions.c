3449c5701301e021ab1c202315ba83dc4122b6f6
fatheddine-bicane <fatheddine04@gmail.com>
2025-06-25 14:37:07 +0100

    feat: a precise sleep function and a function to get time of day

---
+++ philo/utils/utils/time_functions.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_functions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 11:19:45 by fbicane           #+#    #+#             */
+/*   Updated: 2025/06/25 14:34:24 by fbicane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../philo.h"
+
+long	gettime(void)
+{
+	struct timeval	tv;
+
+	if (0 != gettimeofday(&tv, NULL))
+		return (time_error_1(1), -1);
+	return ((tv.tv_sec * 1e3) + (tv.tv_usec / 1e3));
+}
+
+// INFO: a precise sleep function
+void	ft_sleep(long milliseconds)
+{
+	long	start;
+
+	start = gettime();
+	while ((gettime() - start) < milliseconds)
+		usleep(500);
+}
