00c4db54e673c46b40fd3b8e41f605eada92e50e
fatheddine-bicane <fatheddine04@gmail.com>
2025-06-27 18:26:14 +0100

    fix: remove unnecessary RESET in printf

--- philo/utils/utils/print_status.c
+++ philo/utils/utils/print_status.c
@@ -6,7 +6,7 @@
 /*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 15:24:37 by fbicane           #+#    #+#             */
-/*   Updated: 2025/06/27 10:01:24 by fbicane          ###   ########.fr       */
+/*   Updated: 2025/06/27 18:25:22 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,15 @@ void	print_status(t_philo_stat stat, t_philosopher *philo)
 	curent_time = gettime() - philo->table->start_dinner;
 
 	if (TAKEN_A_FORK == stat && false == end_dinner(philo->table))
-		printf(YELLOW"%-6ld"RESET BLUE" %d"RESET " has taken a fork\n", curent_time, philo->id);
+		printf(YELLOW"%-6ld"BLUE" %d"RESET " has taken a fork\n", curent_time, philo->id);
 	else if (EATING == stat && false == end_dinner(philo->table))
-		printf(YELLOW"%-6ld"RESET BLUE" %d"RESET " is eating\n", curent_time, philo->id);
+		printf(YELLOW"%-6ld"BLUE" %d"RESET " is eating\n", curent_time, philo->id);
 	else if (SLEEPING == stat && false == end_dinner(philo->table))
-		printf(YELLOW"%-6ld"RESET BLUE" %d"RESET " is sleeping\n", curent_time, philo->id);
+		printf(YELLOW"%-6ld"BLUE" %d"RESET " is sleeping\n", curent_time, philo->id);
 	else if (THINKING == stat && false == end_dinner(philo->table))
-		printf(YELLOW"%-6ld"RESET BLUE" %d"RESET " is thinking\n", curent_time, philo->id);
+		printf(YELLOW"%-6ld"BLUE" %d"RESET " is thinking\n", curent_time, philo->id);
 	else if (DIED == stat)
-		printf(YELLOW"%-6ld"RESET BLUE" %d"RESET " died\n", curent_time, philo->id);
+		printf(YELLOW"%-6ld"BLUE" %d" RED" died\n"RESET, curent_time, philo->id);
 
 	pthread_mutex_unlock(&philo->table->write_mutex);
 }
